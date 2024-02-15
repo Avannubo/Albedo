928eecf74ba00acb10ca13daf1d6a07ef222a2cb
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-02-15 18:18:57 +0100

    home

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,45 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start">
+      <div className="flex flex-row h-[80px] w-[1300px] bg-blue-800 justify-start p-8 self-center">
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={100}
+          height={24}
+          priority
+        />
+        <div className="flex grow justify-between self-center space-x-4">
+          <div className="flex justify-start space-x-6">
+            <div>Productos</div>
+            <div>Servicios</div>
+          </div>
+          <div className="w-[200px]">
+            <div>Sobre nosotros</div>
+          </div>
+
+        </div>
+
+        {/* <div>
+          <ul>
+            <li>Sobre nosotros
+              <ul>
+
+                <li>Quienes somos</li>
+                <li>
+                  Contacto 
+                </li>
+                <li>FAQ</li>
+              </ul>
+            </li>
+          </ul>
+        </div> */}
+      </div>
+      <div className="bg-red w-50">
+        this is a test web
+      </div>
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -26,8 +64,7 @@ export default function Home() {
             />
           </a>
         </div>
-      </div>
-
+      </div> 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -37,9 +74,8 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      </div> 
+       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"

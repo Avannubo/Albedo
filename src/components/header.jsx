403ceaf896b575690aa302e51a1df55c354ef60a
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-03-08 13:10:50 +0100

    fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import { getCategories } from "@/lib/data"
+import Image from "next/image"; 
 
-export default async function header() {
-    const categories = await getCategories();
 
-    return <header className="fixed top-0 left-0 w-full">
+function header() {
+    return (
+        <header className="fixed top-0 left-0 w-full">
             <div className="flex flex-col items-center justify-start text-white bg-[#304590]">
                 <div className="flex flex-row h-[80px] w-[1100px] p-4 self-center"> 
                         <Image src="/images/Logo_albedo_blanco.png"
@@ -16,17 +15,7 @@ export default async function header() {
                         />
                         <div className="flex grow justify-between self-center space-x-4">
                             <div className="flex justify-start space-x-6">
-                                <ul>Productos
-                                {
-                                    categories.map(category => {
-                                        const {
-                                            name
-                                        } = category; 
-                        
-                                        return <li>{ name }</li>
-                                    })
-                                } 
-                                </ul>
+                                <div>Productos</div>
                                 <div>Servicios</div>
                             </div>
                             <div className="w-auto">
@@ -36,4 +25,6 @@ export default async function header() {
                     </div> 
             </div>
         </header>
+    );
 }
+export default header;

b9d5ade67975f1431ea6ccd25aebc15289f67675
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-03-11 18:29:28 +0100

    admin

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,15 +1,23 @@
-import { getCategories } from "@/lib/data"
+import { getCategories } from "@/lib/data";
 
-export default async function page() {
+export default async function Page() {
     const data = await getCategories();
-    return <div>
-        {data.map(category => {
-            return <div key={category.id}>
-                <p>{category.id} <br/></p>
-                {/* {category.subCategories} */}
+    return (
+        <div>
+            <h1>Products</h1>
+            {data.map(category => (
+                <div key={category.id} className="p-2">
+                    <p className="h-auto w-[400px] bg-slate-200 rounded-lg m-4 p-2">{category.id}</p>
+                    {category.subCategories.map(subCategory => (
+                        <div key={subCategory.id} className="h-auto w-[400px] bg-purple-200 rounded-lg ml-20 m-4 p-2">
+                            <p>{subCategory.id}</p>
+                        </div>
+                    ))}
+                    <div></div>
 
-            </div>
-        })
-        }
-    </div>;
+
+                </div>
+            ))}
+        </div>
+    );
 }

9e645da0c1bc1c6ffac78ff7c94c36814e7bb769
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-03-19 16:07:11 +0100

    addproductmodal1

--- src/app/admin/categories/page.jsx
+++ src/app/admin/categories/page.jsx
@@ -2,8 +2,8 @@ import { getCategories } from "@/lib/data";
 import Modal from '@/components/admin/categories/deleteModal'; 
 import Link from "next/link";
 import Delete from "@/components/admin/categories/delete";
-import AddCategory from "@/components/admin/categories/addsubcategory/add";
-import AddModal from '@/components/admin/categories/addnewcategory/add';
+import AddSubCategory from "@/components/admin/categories/addsubcategory/add";
+import AddNewCategoryModal from '@/components/admin/categories/addnewcategory/add';
 
 
 const Category = ({ category }) => (
@@ -18,7 +18,7 @@ const Category = ({ category }) => (
           </svg>
         </div>
 
-        <AddCategory categoryId={category}/>
+        <AddSubCategory categoryId={category}/>
         <Link href={`/admin/categories/${category.id}`} className=" rounded-md hover:bg-yellow-300 cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
@@ -62,7 +62,7 @@ export default async function Page() {
     <div className="">
       <div className="flex mb-8">
         <h1 className="font-semibold text-4xl">Categorias</h1>
-        <AddModal />
+        <AddNewCategoryModal />
       </div>
       
       {data.map((category) => (

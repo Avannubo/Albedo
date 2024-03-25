2d81faa5c9ffa4cc928b2ec93f503e63caae430d
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-03-25 17:50:19 +0100

    buildchange1

--- next.config.js
+++ next.config.js
@@ -1,22 +1,24 @@
 // next.config.js
-export function webpack(config, { isServer }) {
-    if (!isServer) {
+module.exports = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
         config.node = {
-            fs: 'empty' // This is required to prevent errors from Next.js during the build
+          fs: 'empty' // This is required to prevent errors from Next.js during the build
         };
-    }
-    config.module.rules.push({
+      }
+      config.module.rules.push({
         test: /\.(png|jpe?g|gif|mp4|mov|ogg|webm)$/i,
         use: [
-            {
-                loader: 'file-loader',
-                options: {
-                    publicPath: '/_next',
-                    name: 'public/data/data.json'
-                }
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: '/_next',
+              name: 'public/data/data.json'
             }
+          }
         ]
-    });
-    return config;
-}
+      });
+      return config;
+    }
+  };
   

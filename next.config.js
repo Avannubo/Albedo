3823a196e6eb4f2e030d1e03d87e85f4d6418e2c
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-03-25 17:53:54 +0100

    buildchange3

--- next.config.js
+++ next.config.js
@@ -1,23 +1,22 @@
 // next.config.js
 module.exports = {
     webpack: (config, { isServer }) => {
+      // For the server, we don't need to do anything special
       if (!isServer) {
-        config.node = {
-          fs: 'empty' // This is required to prevent errors from Next.js during the build
-        };
-      }
-      config.module.rules.push({
-        test: /\.(png|jpe?g|gif|mp4|mov|ogg|webm)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next',
-              name: '/public/data/data.json'
+        // Define a rule for handling static assets like images
+        config.module.rules.push({
+          test: /\.(png|jpe?g|gif|mp4|mov|ogg|webm)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                publicPath: '/_next',
+                name: '/public/data/data.json'
             }
-          }
-        ]
-      });
+            }
+          ]
+        });
+      }
       return config;
     }
   };

310825c515b02f0a5d219656e88a766a821efe02
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-04-05 14:16:12 +0200

    isPublishedtext

--- src/app/(admin)/admin/categories/page.jsx
+++ src/app/(admin)/admin/categories/page.jsx
@@ -9,17 +9,21 @@ import EditCatedory from "@/components/admin/categories/category/edit";
 
 const Category = ({ category }) => (
   <div key={category.id} className="space-y-2 w-full">
-    <div className="bg-slate-300 rounded-lg p-2 flex flex-row justify-between mb-2 mt-4">
-      <p className="h-auto ">{category.url_Id} : {category.name}</p>
-      <div className="space-x-4 flex flex-row"> 
-        {/* NewProduct svg */} 
-        <AddNewProduct categoryId={category}/> 
+    <div className="bg-slate-300 rounded-lg p-2 flex flex-row justify-between mb-2 mt-4 hover:shadow-md">
+      <p className="h-auto  self-center">{category.url_Id} : {category.name}</p>
+      <div className="space-x-4 flex flex-row">
+        {/* <p>{category.isPublished ? "Publicado" : "Oculto"}</p> */}
+        
+        <AddNewProduct categoryId={category} />
         {/* addSubCat svg */}
-        <AddSubCategory categoryId={category}/>
+        <AddSubCategory categoryId={category} />
         {/* edit svg */}
-        <EditCatedory categoryId={category}/> 
+        <EditCatedory categoryId={category} />
         {/* delete svg */}
         <Delete categoryId={category} productId={"none"} />
+        <p className={`flex justify-center px-2 py-1 rounded-full w-[100px]  ${category.isPublished ? 'select-none font-medium text-green-600' : 'select-none font-medium text-red-600'}`}>
+          {category.isPublished ? "Publicado" : "Oculto"}
+        </p>
       </div>
     </div>
     {category.products && category.products.length > 0 && (
@@ -27,14 +31,18 @@ const Category = ({ category }) => (
         {category.products.reverse().map((product) => (
           <div
             key={product.ALBEDOcodigo}
-            className="ml-14 flex flex-row justify-between bg-slate-200 rounded-lg p-2 mb-2"
+            className="ml-14 flex flex-row justify-between bg-slate-200 rounded-lg p-2 mb-2 hover:shadow-md"
           >
-            <p className="h-auto w-full  ">
+            <p className="h-auto w-full self-center ">
               {product.url_Id} : {product.ALBEDOtitulo}
             </p>
             <div className="space-x-4 flex flex-row">
-              <EditProduct productId={product.ALBEDOcodigo}/> 
-              <Delete categoryId={"none"} productId={product} />  
+              
+              <EditProduct productId={product.ALBEDOcodigo} />
+              <Delete categoryId={"none"} productId={product} />
+              <p className={`flex justify-center  px-2 py-1 rounded-full w-[100px] ${product.isPublished ? 'select-none font-medium  text-green-600' : 'select-none font-medium text-red-600'}`}>
+                {product.isPublished ? "Publicado" : "Oculto"}
+              </p>
             </div>
           </div>
         ))}
@@ -42,7 +50,7 @@ const Category = ({ category }) => (
     )}
     {category.subCategories &&
       category.subCategories.length > 0 &&
-      category.subCategories.reverse().map((subCategory) => (
+      category.subCategories.map((subCategory) => (//reverse()
         <div key={subCategory.id} className="ml-14">
           <Category category={subCategory} />
         </div>
@@ -55,7 +63,7 @@ export default async function Page() {
   // data.reverse();
   // console.log(data.reverse());
   return (
-    <div className="">
+    <div className="ml-28">
       <div className="flex mb-8">
         <h1 className="font-semibold text-4xl">Productos</h1>
         <AddNewCategoryModal />
--- src/components/admin/categories/category/add.jsx
+++ src/components/admin/categories/category/add.jsx
@@ -9,7 +9,7 @@ export default function addCategory() {
         setIsModalOpen(!isModalOpen);
     };
     return (
-        <div>
+        <div className='self-center'>
             <div onClick={toggleModal} className="cursor-pointer flex flex-row rounded-lg ml-4 p-2 bg-blue-300 hover:bg-blue-400 self-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />

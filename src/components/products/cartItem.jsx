31a5ea9bd4eea99209660ef120d6fb80bd7b3780
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-04-10 12:27:16 +0200

    dynaRouteingfinished2

--- src/components/products/cartItem.jsx
+++ src/components/products/cartItem.jsx
@@ -2,23 +2,22 @@
 import React from "react";
 
 export default function cartItem() {
-  const cartItems = JSON.parse(localStorage.getItem("carrito")) || [];
-  console.log(cartItems);
-  
+  // const cartItems = JSON.parse(localStorage.getItem("carrito")) || [];
+  // console.log(cartItems);
   //Borrar elemento del carrito
 
-  const deleteCartProduct = (id) => {
-    const nuevoCarrito = cartItems.filter(
-      (product) => product.ALBEDOcodigo !== id
-    );
-    localStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
+  // const deleteCartProduct = (id) => {
+  //   const nuevoCarrito = cartItems.filter(
+  //     (product) => product.ALBEDOcodigo !== id
+  //   );
+  //   localStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
 
-    window.location.reload();
-  };
+  //   window.location.reload();
+  // };
 
   return (
     <div>
-      {cartItems && cartItems.length > 0 ? (
+      {/* {cartItems && cartItems.length > 0 ? (
         <div>
           {cartItems.map((product) => (
             <div className="rounded-lg mb-2">
@@ -72,63 +71,63 @@ export default function cartItem() {
         </div>
       ) : (
         <h1 className="">No hay productos en el carrito</h1>
-      )}
+      )} */}
     </div>
   );
 }
 
 {
-  <div className="rounded-lg space-y-2">
-    <div className="justify-between  border bg-white p-6  sm:flex sm:justify-start">
-      <img
-        src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        alt="product-image"
-        className="w-full sm:w-40"
-      />
-      <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-        <div className="mt-5 sm:mt-0">
-          <h2 className="text-lg font-bold text-gray-900">
-            Nike Air Max 2019
-          </h2>
-          <p className="mt-1 text-xs text-gray-700">36EU - 4US</p>
-        </div>
-        <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-          <div className="flex items-center border-gray-100">
-            <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
-              {" "}
-              -{" "}
-            </span>
-            <input
-              className="h-8 w-8 border bg-white text-center text-xs outline-none"
-              type="number"
-              value=""
-              min="1"
-            />
-            <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
-              {" "}
-              +{" "}
-            </span>
-          </div>
-          <div className="flex items-center space-x-4">
-            <p className="text-sm">259.000 €</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </div>
+  /* <div className="rounded-lg space-y-2">
+                <div className="justify-between  border bg-white p-6  sm:flex sm:justify-start">
+                  <img
+                    src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                    alt="product-image"
+                    className="w-full sm:w-40"
+                  />
+                  <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                    <div className="mt-5 sm:mt-0">
+                      <h2 className="text-lg font-bold text-gray-900">
+                        Nike Air Max 2019
+                      </h2>
+                      <p className="mt-1 text-xs text-gray-700">36EU - 4US</p>
+                    </div>
+                    <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                      <div className="flex items-center border-gray-100">
+                        <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                          {" "}
+                          -{" "}
+                        </span>
+                        <input
+                          className="h-8 w-8 border bg-white text-center text-xs outline-none"
+                          type="number"
+                          value=""
+                          min="1"
+                        />
+                        <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                          {" "}
+                          +{" "}
+                        </span>
+                      </div>
+                      <div className="flex items-center space-x-4">
+                        <p className="text-sm">259.000 €</p>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
+              </div> */
 }

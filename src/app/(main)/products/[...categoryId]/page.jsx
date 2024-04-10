af0ebbac7bdcf6e9f97432e3bd2468c86ccc6cb4
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-04-10 10:36:19 +0200

    cat-SubcatDone

--- src/app/(main)/products/[...categoryId]/page.jsx
+++ src/app/(main)/products/[...categoryId]/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import useCategoryId from '@/hooks/useCategoryId';
-import { getCategories } from '@/lib/data';
+import { getDataByUrlId } from '@/lib/data';
 import Image from 'next/image';
 import Link from 'next/link';
 // import { useRouter } from 'next/router';
@@ -9,53 +9,55 @@ import Link from 'next/link';
 export default function PageContent() {
     // const router = useRouter();
     // const { slug } = router.query;
-    // console.log(slug);
+    // console.log(slug); 
+    const slugArrayHook = useCategoryId();
+    const [pageData, setPageData] = useState(null);
 
-    const categoryId = useCategoryId();
-    const [category, setCategory] = useState(null); 
+    function formateSlugArray(slugArrayHook) {
+        if (!Array.isArray(slugArrayHook) || slugArrayHook.length === 0) {
+            return ""; // Return empty string if slugArrayHook is not valid
+        }
+
+        // Join the slugArrayHook elements into a string with '/' as the delimiter
+        return slugArrayHook.join("/");
+    }
     useEffect(() => {
         async function fetchData() {
             try {
-                const data = await getCategories();
-                console.log(categoryId); 
-
-
-
-                    const foundCategory = data.find(cat => cat.url_Id === categoryId[0].toString());
-                    if (foundCategory) {
-                        const subcatIds = foundCategory.subCategories[0].url_Id;
-                        console.log(subcatIds);
-                        setCategory(foundCategory);
-                } 
-                
-
-
-
+                const data = await getDataByUrlId(slugArrayHook);
+                console.log(data);
+                // const foundCategory = data.find(cat => cat.url_Id === slugArrayHook[0].toString());
+                // if (foundCategory) {
+                // const subcatIds = foundCategory.subCategories[0].url_Id;
+                // console.log(subcatIds);
+                setPageData(data);
+                // } 
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
         }
         fetchData();
-    }, [categoryId]);
-    return (
-        <div className='min-h-[50vh]'>
+    }, [slugArrayHook]);
+
+    if (pageData) {
+        return <div className='min-h-[50vh]'>
             <hr className="h-1 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700" />
-            {category && (
+            {pageData && (
                 <div className='py-4'>
                     <div className='flex justify-center my-4'>
                         <h1 className='text-2xl font-bold'>
-                            {category.name}
+                            {pageData.name}
                         </h1>
                     </div>
                     <div className='flex justify-center my-4'>
                         <h1 className='text-lg font-normal'>
-                            {category.ALBEDOdescripcion}
+                            {pageData.ALBEDOdescripcion}
                         </h1>
                     </div>
                     <div className='flex justify-center my-2'>
-                        {category.ALBEDOcuerpo}
+                        {pageData.ALBEDOcuerpo}
                     </div>
-                    {category.subCategories && category.subCategories.length > 0 && (
+                    {pageData.subCategories && pageData.subCategories.length > 0 && (
                         <div>
                             <hr className="h-1 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700" />
                             <div className=' flex justify-center my-2'>
@@ -63,8 +65,8 @@ export default function PageContent() {
                             </div>
                             <hr className="h-1 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700" />
                             <div className='flex flex-row flex-wrap justify-center '>
-                                {category.subCategories.map((subCat, index) => (
-                                    <Link href={`/products/${categoryId}/${subCat.url_Id}`} key={index} className=''>
+                                {pageData.subCategories.map((subCat, index) => (
+                                    <Link href={`/products/${formateSlugArray(slugArrayHook)}/${subCat.url_Id}`} key={index} className=''>
                                         <div className='w-[250px] flex flex-col justify-center'>
                                             <Image
                                                 src={subCat.urlImagen}
@@ -82,7 +84,7 @@ export default function PageContent() {
                             </div>
                         </div>
                     )}
-                    {category.products && category.products.length > 0 && (
+                    {pageData.products && pageData.products.length > 0 && (
                         <div>
                             <hr className="h-1 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700" />
                             <div className=' flex justify-center my-2'>
@@ -90,8 +92,8 @@ export default function PageContent() {
                             </div>
                             <hr className="h-1 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700" />
                             <div className='flex flex-row justify-center space-x-6'>
-                                {category.products.map((product, index) => (
-                                    <Link href={`/products/${categoryId}/${product.url_Id}`}
+                                {pageData.products.map((product, index) => (
+                                    <Link href={`/products/${formateSlugArray(slugArrayHook)}/${product.url_Id}`}
                                         key={index} className='flex flex-col justify-center'>
                                         <Image
                                             src={product.imagen}
@@ -102,7 +104,7 @@ export default function PageContent() {
                                             priority
                                         />
                                         <p className='self-center font-bold'>
-                                            {product.ALBEDOtitulo}</p>
+                                            {product.ALBEDOcodigo}</p>
                                     </Link>
                                 ))}
                             </div>
@@ -111,6 +113,5 @@ export default function PageContent() {
                 </div>
             )}
         </div>
-
-    );
+    }
 }

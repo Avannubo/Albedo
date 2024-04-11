4175065c0a879bdedf08084e8a71e5b947c4a596
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-04-11 18:19:06 +0200

    cartFinished

--- src/app/(main)/checkout/page.jsx
+++ src/app/(main)/checkout/page.jsx
@@ -4,10 +4,83 @@ import CartItem from "@/components/products/cartItem";
 export default function Page() {
   const [selectedShipping, setSelectedShipping] = useState(null);
   const [selectedPayment, setSelectedPayment] = useState(null);
-  const [cartProducts, setCartProducts] = useState();
+  const [cartProducts, setCartProducts] = useState([]);
+  const [userInfo, setUserInfo] = useState({
+    firstName: '',
+    lastName: '',
+    dni: '',
+    dateOfBirth: '',
+    company: '',
+    cif: '',
+    phoneNumber: '',
+    address: '',
+    city: '',
+    province: '',
+    zipCode: ''
+  });
   const handleShippingSelect = (shippingOption) => {
     setSelectedShipping(shippingOption);
     console.log("Selected shipping option:", shippingOption);
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = {};
+    if (!userInfo.firstName.trim()) {
+      errors.firstName = "El nombre es obligatorio.";
+    }
+    if (!userInfo.lastName.trim()) {
+      errors.lastName = "Los apellidos son obligatorios.";
+    }
+    if (!userInfo.dni.trim()) {
+      errors.dni = "El DNI es obligatorio.";
+    }
+    // if (!userInfo.dateOfBirth.trim()) {
+    //   errors.dateOfBirth = "El DNI es obligatorio.";
+    // }
+    if (!userInfo.dni.trim()) {
+      errors.dni = "El DNI es obligatorio.";
+    }
+    // if (!userInfo.company.trim()) {
+    //   errors.company = "El DNI es obligatorio.";
+    // }
+    // if (!userInfo.cif.trim()) {
+    //   errors.cif = "El CIF es obligatorio.";
+    // }
+    if (!userInfo.phoneNumber.trim()) {
+      errors.phoneNumber = "El Número de teléfono es obligatorio.";
+    }
+    if (!userInfo.address.trim()) {
+      errors.address = "El Dirección de envío es obligatorio.";
+    } 
+    if (!userInfo.city.trim()) {
+      errors.city = "El Cuidad es obligatorio.";
+    }
+    if (!userInfo.province.trim()) {
+      errors.province = "El province es obligatorio.";
+    }
+    if (!userInfo.zipCode.trim()) {
+      errors.zipCode = "El zipCode es obligatorio.";
+    }
+    // Add validation for other required fields...
+    // If there are errors, display them and prevent form submission
+    if (Object.keys(errors).length > 0) {
+      console.log(errors);
+      return;
+    }
+    const orderData = {
+      userInfo,
+      selectedShipping,
+      selectedPayment,
+      cartProducts
+    };
+    console.log(orderData);
   };
   const handlePaymentSelect = (paymentMethod) => {
     setSelectedPayment(paymentMethod);
@@ -16,18 +89,17 @@ export default function Page() {
   useEffect(() => {
     const intervalId = setInterval(() => {
       if (typeof window !== 'undefined') {
-        setCartProducts(JSON.parse(localStorage.getItem("carrito")));
+        setCartProducts(JSON.parse(localStorage.getItem("carrito")) || []);
       }
     }, 500);
-    // Clear the interval on component unmount to prevent memory leaks
     return () => clearInterval(intervalId);
   }, []);
-  const subTotal = cartProducts ? cartProducts.reduce((total, product) => total + (product.quantity * product.ALBEDOprecio), 0) : 0;
+  const subTotal = cartProducts.reduce((total, product) => total + (product.quantity * product.ALBEDOprecio), 0);
   return (
     <div className="flex flex-row items-start h-[85vh] w-[1100px] mt-6 mb-8">
       <div className="bg-gray-50 p-2 mr-2 grow w-[45%]">
         <h1 className="mb-4 text-start text-2xl font-bold">Datos del pedido</h1>
-        <form className="w-full max-w-lg">
+        <form onSubmit={handleSubmit} className="w-full max-w-lg" >
           <div className="flex flex-wrap -mx-3 mb-4">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label
@@ -40,6 +112,9 @@ export default function Page() {
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 id="grid-first-name"
                 type="text"
+                name="firstName"
+                value={userInfo.firstName}
+                onChange={handleInputChange}
               />
               {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
             </div>
@@ -54,6 +129,9 @@ export default function Page() {
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-last-name"
                 type="text"
+                name="lastName"
+                value={userInfo.lastName}
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -68,6 +146,9 @@ export default function Page() {
               <input
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="text"
+                name="dni"
+                value={userInfo.dni}
+                onChange={handleInputChange}
               />
             </div>
             <div className="w-full md:w-1/2 px-3">
@@ -75,12 +156,15 @@ export default function Page() {
                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 htmlFor="grid-last-name"
               >
-                Fecha de nacimiento:
+                Fecha de nacimiento (Opcional):
               </label>
               <input
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-last-name"
                 type="date"
+                name="dateOfBirth"
+                value={userInfo.dateOfBirth}
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -95,6 +179,9 @@ export default function Page() {
               <input
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="text"
+                name="company"
+                value={userInfo.company}
+                onChange={handleInputChange}
               />
             </div>
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -107,6 +194,9 @@ export default function Page() {
               <input
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="text"
+                name="cif"
+                value={userInfo.cif}
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -121,6 +211,9 @@ export default function Page() {
               <input
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="telephone"
+                name="phoneNumber"
+                value={userInfo.phoneNumber}
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -135,6 +228,9 @@ export default function Page() {
               <input
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="text"
+                name="address"
+                value={userInfo.address}
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -150,6 +246,9 @@ export default function Page() {
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-city"
                 type="text"
+                name="city"
+                value={userInfo.city}
+                onChange={handleInputChange}
               />
             </div>
             <div className="w-full md:w-1/3 px-3 mb-6">
@@ -163,6 +262,9 @@ export default function Page() {
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-city"
                 type="text"
+                name="province"
+                value={userInfo.province}
+                onChange={handleInputChange}
               />
             </div>
             <div className="w-full md:w-1/3 px-3 mb-6">
@@ -176,6 +278,9 @@ export default function Page() {
                 className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-zip"
                 type="text"
+                name="zipCode"
+                value={userInfo.zipCode}
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -249,9 +354,11 @@ export default function Page() {
                     <p className="text-sm text-gray-700">*IVA incluido</p>
                   </div>
                 </div>
-                <button className="mt-2 w-full rounded-md bg-[#304590] py-1.5 font-medium text-blue-50 hover:bg-[#475caa]">
-                  Proceder al pago
-                </button>
+                <form onSubmit={handleSubmit}>
+                  <button type="submit" className="mt-2 w-full rounded-md bg-[#304590] py-1.5 font-medium text-blue-50 hover:bg-[#475caa]">
+                    Proceder al pago
+                  </button>
+                </form>
               </div>
             </div>
           </div>

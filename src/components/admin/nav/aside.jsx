b370f582ab578d3182d0f5e7ebf1da153a101ae1
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-04-18 11:25:50 +0200

    loginDone

--- src/components/admin/nav/aside.jsx
+++ src/components/admin/nav/aside.jsx
@@ -17,7 +17,7 @@ export default function aside() {
       </Link>
       <div className="flex flex-col space-y-4 p-4 ">
         <Link
-          href="/admin"
+          href="/admin/escritorio"
           className="cursor-pointer p-4 w-full box-shadow bg-white rounded-lg "
         >
           <p className="flex justify-center font-semibold">Escritorio</p>
---
+++ src/hooks/getPass.js
@@ -0,0 +1,5 @@
+
+export default function getpass() {
+    // const pass = process.env.PASSWORD_HASH
+    // return pass;
+}

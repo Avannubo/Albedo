33aa8f84e674e73b9615f0705fa5318fc2d10fc1
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2024-04-22 12:54:44 +0200

    filtersPedidos

--- src/app/(admin)/admin/orders/page.jsx
+++ src/app/(admin)/admin/orders/page.jsx
@@ -1,59 +1,83 @@
+
+"use client"
+import { useState, useEffect } from 'react';
 import React from 'react'
 import Details from '@/components/admin/orders/details/btn'
 import State from '@/components/admin/orders/state/btn'
 import OrdersStateCount from '@/components/admin/orders/OrdersStateCount'
 import Layout from "@/app/(admin)/admin/AdminLayout";
 import { getAllOrders } from '@/lib/data'
-export default async function page() {
-  const orders = await getAllOrders(); 
+export default function page() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [orders, setOrders] = useState();
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    setIsLoading(true);
+    const data = await getAllOrders();
 
+    setOrders(data);
+    setIsLoading(false);
+  }
   return (
     <Layout>
-      <h1 className="font-semibold text-4xl">Pedidos</h1>
-      <div className="flex flex-col ">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <table
-                className="min-w-full text-left text-md font-light text-surface ">
-                <thead
-                  className="border-b border-neutral-200 font-medium ">
-                  <tr>
-                    <th scope="col" className="text-center px-6 py-4">ID</th>
-                    <th scope="col" className="text-center px-6 py-4">First</th>
-                    <th scope="col" className="text-center px-6 py-4">Last</th>
-                    <th scope="col" className="text-center px-6 py-4">DNI</th>
-                    <th scope="col" className="text-center px-6 py-4">Total de Pedido</th>
-                    <th scope="col" className="text-center px-6 py-4">Metodo de Pago</th>
-                    <th scope="col" className="text-center px-6 py-4">Estado</th>
-                    <th scope="col" className="text-center px-6 py-4">Ver</th>
-                  </tr>
-                </thead>
-                <tbody className='border-spacing-y-4'>
-                  {orders && orders.length > 0 && orders.map((order, index) => (
-                    <tr key={index} className="border-b border-neutral-200  rounded-lg">
-                      {/* hover:bg-slate-200 */}
-                      <td className="text-center whitespace-nowrap px-6 py-4 font-medium">#{index + 1}</td>
-                      <td className="text-center whitespace-nowrap px-6 py-4 font-medium">{order.userInfo.firstName}</td>
-                      <td className="text-center whitespace-nowrap px-6 py-4 font-medium">{order.userInfo.lastName}</td>
-                      <td className="text-center whitespace-nowrap px-6 py-4 font-medium">{order.userInfo.dni}</td>
-                      <td className="text-center whitespace-nowrap px-6 py-4 font-medium">{order.totalPedido}€</td>
-                      <td className="text-center whitespace-nowrap px-6 py-4 font-medium">{order.selectedPayment}</td>
-                      <td className="text-center whitespace-nowrap px-6 py-4 font-medium">
-                        <State orderState={order.orderState} orderId={index} />
-                      </td>
-                      <td className="text-center whitespace-nowrap px-6 py-4 font-medium">
-                        <Details orderId={index} />
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+      <div>
+        <h1 className="font-semibold text-4xl">Pedidos</h1>
+        <div>
+          
+        </div>
+      </div>
+      
+      {isLoading ? (
+        <div class="flex-col gap-4 w-full flex items-center justify-center">
+          <div class="w-20 h-20 border-8 text-blue-600 text-xl animate-spin border-gray-300 flex items-center justify-center border-t-blue-600 rounded-full">
           </div>
         </div>
-      </div> 
-      {/* <OrdersStateCount /> */}
+      ) : (
+        <div className="flex flex-col ">
+          <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+              <div className="overflow-hidden">
+                  {orders && orders.length > 0 && (<table
+                    className="min-w-full text-left text-md font-light text-surface ">
+                    <thead
+                      className="border-b border-neutral-200 font-medium ">
+                      <tr>
+                        <th scope="col" className="text-center px-6 py-4">ID</th>
+                        <th scope="col" className="text-center px-6 py-4">First</th>
+                        <th scope="col" className="text-center px-6 py-4">Last</th>
+                        <th scope="col" className="text-center px-6 py-4">DNI</th>
+                        <th scope="col" className="text-center px-6 py-4">Total de Pedido</th>
+                        <th scope="col" className="text-center px-6 py-4">Metodo de Pago</th>
+                        <th scope="col" className="text-center px-6 py-4">Estado</th>
+                        <th scope="col" className="text-center px-6 py-4">Ver</th>
+                      </tr>
+                    </thead>
+                    <tbody className='border-spacing-y-4'>
+                      {orders.map((order, index) => (
+                        <tr key={index} className="border-b border-neutral-200  rounded-lg">
+                          {/* hover:bg-slate-200 */}
+                          <td className="text-center whitespace-nowrap px-6 py-4 font-medium">#{index + 1}</td>
+                          <td className="text-center whitespace-nowrap px-6 py-4 font-medium">{order.userInfo.firstName}</td>
+                          <td className="text-center whitespace-nowrap px-6 py-4 font-medium">{order.userInfo.lastName}</td>
+                          <td className="text-center whitespace-nowrap px-6 py-4 font-medium">{order.userInfo.dni}</td>
+                          <td className="text-center whitespace-nowrap px-6 py-4 font-medium">{order.totalPedido}€</td>
+                          <td className="text-center whitespace-nowrap px-6 py-4 font-medium">{order.selectedPayment}</td>
+                          <td className="text-center whitespace-nowrap px-6 py-4 font-medium">
+                            <State orderState={order.orderState} orderId={index} />
+                          </td>
+                          <td className="text-center whitespace-nowrap px-6 py-4 font-medium">
+                            <Details orderId={index} />
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>)}
+              </div>
+            </div>
+          </div>
+        </div>)}
     </Layout>
   );
 }
--- src/app/(admin)/admin/products/page.jsx
+++ src/app/(admin)/admin/products/page.jsx
@@ -10,6 +10,7 @@ import EditProduct from "@/components/admin/products/product/edit";
 import EditCatedory from "@/components/admin/products/category/edit";
 import Layout from "@/app/(admin)/admin/AdminLayout";
 const Category = ({ category }) => (
+
   <div key={category.id} className="space-y-2 w-full">
     <div className="border bg-slate-50 rounded-lg p-2 flex flex-row justify-between mb-2 mt-4">
       <p className="h-auto  self-center">{category.url_Id} : {category.name}</p>
@@ -63,6 +64,7 @@ export default function Page() {
   const [categories, setCategories] = useState([]);
   const [filters, setFilters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     fetchData();
   }, [isPublishedFilter, categoryFilter]); // Fetch data on filter change
